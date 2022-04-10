0611ea2f2a576dd6e490b766500af2d984ab6d77
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-10 13:35:12 +0300

    set types: union, explicit

--- lesson-1/sandbox.js
+++ lesson-1/sandbox.js
@@ -1,5 +1,31 @@
-var character = 'luigi';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) { return console.log(input); });
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+// age = 'luigi'
+age = 39;
+// isLoggedIn = 25
+isLoggedIn = true;
+//arrays
+var ninjas = [];
+// ninjas = ['yoshi', 'mario']
+ninjas.push('shaun');
+// union type
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
+// objects
+var ninja1;
+ninja1 = { name: 'yoshi', age: 20 };
+var ninja2;
+ninja2 = {
+    name: 'mario',
+    age: 44,
+    beltColor: 'black'
+};
+console.log(ninja2);
--- lesson-1/sandbox.ts
+++ lesson-1/sandbox.ts
@@ -1,9 +1,45 @@
-const character = 'luigi'
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
 
-console.log(character);
+// age = 'luigi'
+age = 39
 
-const inputs = document.querySelectorAll('input')
+// isLoggedIn = 25
+isLoggedIn = true
 
-console.log(inputs);
+//arrays
+let ninjas: string[] = [];
 
-inputs.forEach(input => console.log(input))
+// ninjas = ['yoshi', 'mario']
+ninjas.push('shaun')
+
+// union type
+let mixed: (string|number|boolean)[] = [];
+mixed.push('hello')
+mixed.push(20)
+mixed.push(false)
+console.log(mixed);
+
+let uid: string|number;
+uid = '123'
+uid = 123
+
+// objects
+let ninja1: object;
+ninja1 = { name: 'yoshi', age: 20 }
+
+let ninja2: {
+  name: string,
+  age: number,
+  beltColor: string
+}
+
+ninja2 = {
+  name: 'mario',
+  age: 44,
+  beltColor: 'black'
+}
+
+console.log(ninja2);
