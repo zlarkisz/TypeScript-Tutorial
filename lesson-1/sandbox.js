16a147575c713c946ed3cd579a3b6413e3b7da0d
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-10 13:25:09 +0300

    cant change structure of arr or obj

--- lesson-1/sandbox.js
+++ lesson-1/sandbox.js
@@ -1,5 +1,25 @@
-var character = 'luigi';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) { return console.log(input); });
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(3);
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun')
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// Objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 50
+};
+ninja.age = 23;
+ninja.name = 'ryu';
+// ninja.age = '30'
+// ninja.skills = ['fighting', 'sneaking']
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 34
+};
--- lesson-1/sandbox.ts
+++ lesson-1/sandbox.ts
@@ -1,9 +1,34 @@
-const character = 'luigi'
+let names = ['luigi', 'mario', 'yoshi'];
 
-console.log(character);
+names.push('toad');
+// names.push(3);
 
-const inputs = document.querySelectorAll('input')
+let numbers = [10, 20, 30, 40]
 
-console.log(inputs);
+numbers.push(25)
+// numbers.push('shaun')
 
-inputs.forEach(input => console.log(input))
+let mixed = ['ken', 4, 'chun-li', 8, 9]
+
+mixed.push('ryu')
+mixed.push(10)
+mixed[0] = 3
+
+// Objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 50
+}
+
+ninja.age = 23
+ninja.name = 'ryu'
+// ninja.age = '30'
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 34,
+  // skills: ['fighting', 'sneaking']
+}
