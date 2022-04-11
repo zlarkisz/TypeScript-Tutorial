9e87e0c68c3883f9037d00a9f6c891a3ce7a88fb
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-11 11:27:58 +0300

    Interfaces

--- lesson-1/public/app.js
+++ lesson-1/public/app.js
@@ -1,3 +1,19 @@
+const me = {
+    name: 'shaun',
+    age: 25,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log(`I spend ${amount}`);
+        return amount;
+    },
+};
+const greetPerson = (person) => {
+    console.log(`hello ${person.name}`);
+};
+greetPerson(me);
+console.log(me);
 import { Invoice } from './classes/invoice.js';
 const invOne = new Invoice('mario', 'work on thw mario website', 350);
 const invTwo = new Invoice('luigi', 'work on thw luigi website', 450);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+// interfaces
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: 'shaun',
+  age: 25,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log(`I spend ${amount}`);
+    return amount;
+  },
+};
+
+const greetPerson = (person: IsPerson) => {
+  console.log(`hello ${person.name}`);
+}
+
+greetPerson(me)
+
+console.log(me);
+
 import { Invoice } from './classes/invoice.js'
 
 const invOne = new Invoice('mario', 'work on thw mario website', 350)
