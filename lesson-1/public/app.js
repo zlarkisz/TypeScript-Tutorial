7f0f270d27146004e31f1febc93e01446900020b
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-11 11:20:23 +0300

    Modules

--- lesson-1/public/app.js
+++ lesson-1/public/app.js
@@ -1,18 +1,4 @@
-"use strict";
-//classes
-class Invoice {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes ${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/invoice.js';
 const invOne = new Invoice('mario', 'work on thw mario website', 350);
 const invTwo = new Invoice('luigi', 'work on thw luigi website', 450);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,4 @@
-//classes
-class Invoice {
-  // readonly client: string;
-  // private details: string;
-  // public amount: number;
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    return `${this.client} owes ${this.amount} for ${this.details}`
-  }
-}
+import { Invoice } from './classes/invoice.js'
 
 const invOne = new Invoice('mario', 'work on thw mario website', 350)
 const invTwo = new Invoice('luigi', 'work on thw luigi website', 450)
